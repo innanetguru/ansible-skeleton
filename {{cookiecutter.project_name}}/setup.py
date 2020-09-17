b91f92f71997579874fe318159eca02842bc4296
innanetguru <dsoprano20@gmail.com>
2020-09-17 15:16:03 +0000

    setup changes

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 ## script to setup local environment and install packages
 ## execute from ./setup.sh
 ## 1) Install python-pip
@@ -10,40 +10,45 @@ import os
 import os.path 
 import subprocess 
 
-home = os.getenv('HOME')
-local_pip_bin = '/.local/bin/pip'
-root_pip_bin = ['/usr/bin/pip','/usr/local/bin/pip']
-user_pip_bin = home + local_pip_bin + "\n"
+
+def configure_pipenv():
+    return
+
+def config_shell(*args, **kwds):
+    # configure bash $PATH variable to include "/$HOME/.local/bin"
+
+    return 
+
+def install_pip():
+        #install pip
+    
+    return config_shell()
 
 # discover whether pip is currently installed and where
-def discover_pip():
+def discover_pip(*args):
+
     check_pip = subprocess.check_output('which pip; exit 0', shell=True, stderr=subprocess.STDOUT)
 
     if check_pip == user_pip_bin:
         print('pip is installed in the home directory')
         print(check_pip)
+        return config_shell()
     elif check_pip in root_pip_bin:
         print('pip is enabled for users with sudo/root permissions')
         print(check_pip)
     else:   # i.e. if check_pip != user_bin_pip or root_pip_bin
         # declare global namespace 'pip' with value 'None' to pass to install_pip
-        global pip; pip = None
-    
-def install_pip(*args, **kwds):
-    if "pip" in args:
-        pip=args["pip"]
-        #install pip
+        #global pip; 
+        return install_pip()
     
 
-def config_shell(*args, **kwds):
-    if pip in args:
-        print(pip)
-    else:
+home = os.getenv('HOME')
+local_pip_bin = '/.local/bin/pip'
+root_pip_bin = ['/usr/bin/pip','/usr/local/bin/pip']
+user_pip_bin = home + local_pip_bin + "\n"
 
-        return 
-
-def configure_pipenv():
-    return
+def main():
+    discover_pip(root_pip_bin, user_pip_bin, home, local_pip_bin)
 
 if __name__ == "__main__":
-    discover_pip()
+    main()
